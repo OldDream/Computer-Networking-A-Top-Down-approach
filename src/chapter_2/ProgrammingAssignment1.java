00b34ac548fbbd845642766865d0b362d50bade7
OldDream <huangyn15@qq.com>
2017-05-17 12:30:35 +0800

    5.17

--- src/chapter_2/ProgrammingAssignment1.java
+++ src/chapter_2/ProgrammingAssignment1.java
@@ -10,49 +10,48 @@ import java.net.Socket;
 import java.util.StringTokenizer;
 
 public class ProgrammingAssignment1 {
-	private static final int port = 6666;
+    private static final int port = 6666;
 
-	public static void main(String[] args) throws Exception {
-		String requestMessageLine;
-		String fileName;
+    public static void main(String[] args) throws Exception {
+        String requestMessageLine;
+        String fileName;
 
-		ServerSocket listenSocket = new ServerSocket(port);
-		Socket connectionSocket = listenSocket.accept();
+        ServerSocket listenSocket = new ServerSocket(port);
+        Socket connectionSocket = listenSocket.accept();
 
-		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-		requestMessageLine = inFromClient.readLine();
-		StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
+        requestMessageLine = inFromClient.readLine();
+        StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
 
-		if (tokenizedLine.nextToken().equals("GET")) {
-			fileName = tokenizedLine.nextToken();
-			if (fileName.startsWith("/") == true) {
-				fileName = fileName.substring(1);
-				File file = new File(fileName);
-				int numOfBytes = (int)file.length();
-				
-				FileInputStream inFile = new FileInputStream(fileName);
-				byte[] fileInBytes = new byte[numOfBytes];
-				inFile.read(fileInBytes);
-				
-				outToClient.writeBytes("HTTP/1.0 200 Document Follows \r\n");
-				
-			}
-			else {
-				System.out.println("Bad Request Message.");
-			}
-		}
+        if (tokenizedLine.nextToken().equals("GET")) {
+            fileName = tokenizedLine.nextToken();
+            if (fileName.startsWith("/") == true) {
+                fileName = fileName.substring(1);
+                File file = new File(fileName);
+                int numOfBytes = (int) file.length();
 
-		/*
-		 * while (true) { Socket connectionSocket = welcomeSocket.accept();
-		 * BufferedReader inFromClient = new BufferedReader(new
-		 * InputStreamReader(connectionSocket.getInputStream()));
-		 * 
-		 * clientSentence = inFromClient.readLine();
-		 * System.out.println(clientSentence + "\n"); }
-		 */
-	}
+                FileInputStream inFile = new FileInputStream(fileName);
+                byte[] fileInBytes = new byte[numOfBytes];
+                inFile.read(fileInBytes);
+
+                outToClient.writeBytes("HTTP/1.0 200 Document Follows \r\n");
+                if (fileName.endsWith(".jpg"))
+                    outToClient.writeBytes("Content-Type:image/jpeg\r\n");
+                if (fileName.endsWith(".gif"))
+                    outToClient.writeBytes("Content-Type:image/gif\r\n");
+                outToClient.writeBytes("Content-Length: " + numOfBytes + "\r\n");
+                outToClient.writeBytes("\r\n");
+                outToClient.write(fileInBytes, 0, numOfBytes);
+                connectionSocket.close();
+            }
+            else {
+                System.out.println("Bad Request Message.");
+            }
+        }
+        listenSocket.close();
+    }
 }
 
-/*Move to Gt70 test*/
+/* Move to Gt70 test */
